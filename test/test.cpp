3977592d7e38c97b65702c832a3417aed63d7155
grxuser <grxuser@cc07>
2016-02-09 03:35:03 +0900

    add test code

---
+++ test/test.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstring>
+
+#ifdef USE_CNOID_MODEL
+#include <creeklib/model/cnoidBody.hpp>
+#elif defined USE_HRP_MODEL
+#include <creeklib/model/hrpBody.hpp>
+#endif
+#include <creeklib/model/modelLoader.hpp>
+
+void split()
+{
+  std::cout << std::endl << "--------------------------------------------------------------------------" << std::endl << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+  std::string path("/opt/grx/HRP2A/model/HRP2Amain.wrl");
+
+
+  creek::BodyPtr robot( new creek::Body() );
+  if( !creek::loadBody(robot, path) ) {
+    std::cerr << "error : model loader" << std::endl;
+    return 0;
+  }
+  std::cout << robot->name() << std::endl;
+  std::cout << "  " << robot->numJoints() << std::endl;
+  std::cout << "  " << robot->numLinks() << std::endl;
+
+
+  split();
+
+
+  creek::JointPathPtr arm;
+  arm = robot->getJointPath(robot->link("CHEST_JOINT1"), robot->link("RARM_JOINT6"));
+  for(int i=0; i<arm->numJoints(); i++)
+    std::cout << "  " << arm->joint(i)->name() << std::endl;
+
+
+  split();
+
+
+  double half_deg[] = {15,-10,0,-30, 80,0,0};
+  for(int i=0; i<arm->numJoints(); i++)
+    arm->joint(i)->q() = ( half_deg[i] / 180.0 * M_PI );
+  arm->calcForwardKinematics();
+
+  std::cout << arm->endLink()->R() << std::endl;
+  std::cout << arm->endLink()->R().transpose() << std::endl;
+
+  return 0;
+}
