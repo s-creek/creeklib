06ef9de96e2274b48a0ffd39a17c0186aeda0d71
s-creek <empty@email>
2019-07-15 12:57:00 +0900

    add XMeans.hpp : todo impl

---
+++ sclib/include/scl/clustering/XMeans.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file XMeans.hpp
+ */
+
+#ifndef SCL_X_MEANS_HPP
+#define SCL_X_MEANS_HPP
+
+namespace scl
+{
+    /** 
+     * @class XMeans
+     * @brief x-means clustering.
+     * @details 参考サイト @n
+     * <a href="https://qiita.com/deaikei/items/8615362d320c76e2ce0b">qiita</a>
+     */
+  class XMenas
+  {
+  public:
+    /** @brief コンストラクタ */
+    XMeans();
+
+    
+  private:
+    
+  };  // end of x-means class
+  
+}  // end of namespace scl
+
+
+#endif  /* SCL_X_MEANS_HPP */
