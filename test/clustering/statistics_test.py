0c9c8c6a60464435b00916abaab7e22fa89821ec
ogawa <empty>
2019-07-16 18:43:19 +0900

    update examples

---
+++ test/clustering/statistics_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+from scipy import stats
+
+
+### main
+#
+if __name__ == '__main__':
+
+    dataset = np.loadtxt("./log/sample_data.log")
+
+    cov = np.cov(dataset.T)
+    print "cov :"
+    print cov
+
+    x = dataset[0]
+    print "x : ", x
+
+    mean = np.mean(dataset, axis=0)
+    print "mean : ", mean
+
+    num = dataset.shape[0]
+    like = [ stats.multivariate_normal.logpdf(dataset[i], mean, cov) for i in range(num) ]
+    print like
+
+    ret = sum(like)
+    print "-----------------"
+    print "result"
+    print "  ", ret
---
+++ test/clustering/xmeans_test.cpp
@@ -0,0 +1,76 @@
+#include <scl/clustering/XMeans.hpp>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#define PRINT_MAT(X) std::cout << #X << ":\n" << X << std::endl << std::endl
+
+int main ()
+{
+    // file open
+    std::ifstream file("./log/sample_data.log");
+    if ( !file )
+    {
+        return 0;
+    }
+
+
+    // load data
+    std::vector< std::vector<double> > dataset;
+    std::string line;
+    while ( std::getline(file, line) )
+    {
+        if ( line.size() > 1 )
+        {
+            std::stringstream ss(line);
+            std::vector<double> data;
+            double tmp(0);
+            while ( !ss.eof() )
+            {
+                ss >> tmp;
+                data.push_back(tmp);
+            }
+            dataset.push_back(data);
+        }
+    }
+    file.close();
+
+    
+    const std::size_t num(dataset.size()), dim(dataset.front().size());
+    Eigen::MatrixXd mat(num, dim);
+    // for (std::size_t i = 0; i < num; ++i)
+    // {
+    //     for (std::size_t j = 0; j < dim; ++j)
+    //     {
+    //         mat(i,j) = dataset[i][j];
+    //     }
+    // }
+    scl::convertStlToEigen(dim, dataset, mat);
+    
+    
+    Eigen::MatrixXd covariance;
+    scl::calcCovariance(mat, covariance);
+    PRINT_MAT(covariance);
+    
+
+    Eigen::VectorXd x(mat.row(0));
+    Eigen::VectorXd mean(mat.colwise().mean());
+    PRINT_MAT(x);
+    PRINT_MAT(mean);
+        
+
+    for (std::size_t i = 0; i < num; ++i)
+    {
+        double pdf = scl::calcPdf(mat.row(i), mean, covariance);
+        std::cout << " " << log(pdf);
+    }
+    std::cout << std::endl;
+
+
+    double log_likelihood = scl::calcLogLikelihood(mat);
+    std::cout << "------------\nresult\n  " << log_likelihood << std::endl; 
+
+    return 0;
+}
