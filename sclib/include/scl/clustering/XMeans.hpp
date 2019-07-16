e17b2d4903441cfdca5ba39e705cb02a37de6ded
ogawa <empty>
2019-07-16 20:04:12 +0900

    update XMeans

--- sclib/include/scl/clustering/XMeans.hpp
+++ sclib/include/scl/clustering/XMeans.hpp
@@ -27,9 +27,10 @@ namespace scl
     
         /**
          * @brief k-means のパラメータ設定
+         * @param[in] method クラスタ重心の初期化方法 (デフォルト k-means++)
          * @see KMeans::setParameters
          */
-        void setParameters(const std::size_t max_iteration, const double tolerance, const std::size_t max_pp_trial=3);
+        void setParameters(const std::size_t max_iteration, const double tolerance, const std::size_t max_pp_trial=3, const KMeans::InitMethod method=KMeans::PLUSPLUS);
 
 
         /**
@@ -39,18 +40,22 @@ namespace scl
          * @param[in] data_set クラスタリングするデータセット
          * @param[in] init_num_clusters 初期クラスタ数
          * @param[in,out] centroids 各クラスタの重心位置 ( method が KMeans::MANUAL の時だけ[in]も使う)
-         * @param[in] method クラスタ重心の初期化方法 (デフォルト k-means++)
          * @return クラスタリングが収束したか
          * @details DataType needs [] access operator
          */
         template<class DataType>
-        bool clustering(const std::size_t dim, const std::vector<DataType> &data_set, const std::size_t init_num_clusters, std::vector< std::vector<double> > &centroids, const KMeans::InitMethod method=KMeans::PLUSPLUS);
+        bool clustering(const std::size_t dim, const std::vector<DataType> &data_set, const std::size_t init_num_clusters, std::vector< std::vector<double> > &centroids);
 
     
     private:
         /** @brief k-means class */
         scl::KMeans m_kmeans;
-    
+
+        
+        /** @brief k-means method */
+        KMeans::InitMethod m_method;
+
+        
     };  // end of x-means class
 
 
@@ -61,20 +66,22 @@ namespace scl
     //------------------------------------------------------------------
 
     XMeans::XMeans()
+        : m_method(KMeans::PLUSPLUS)
     {
     }
-
-
-    void XMeans::setParameters(const std::size_t max_iteration, const double tolerance, const std::size_t max_pp_trial)
+    
+    
+    void XMeans::setParameters(const std::size_t max_iteration, const double tolerance, const std::size_t max_pp_trial, const KMeans::InitMethod method)
     {
         m_kmeans.setParameters(max_iteration, tolerance, max_pp_trial);
+        m_method = method;
     }
-
-
+    
+    
     template<class DataType>
-    bool XMeans::clustering(const std::size_t dim, const std::vector<DataType> &data_set, const std::size_t init_num_clusters, std::vector< std::vector<double> > &centroids, const KMeans::InitMethod method)
+    bool XMeans::clustering(const std::size_t dim, const std::vector<DataType> &data_set, const std::size_t init_num_clusters, std::vector< std::vector<double> > &centroids)
     {
-        m_kmeans.clustering(dim, data_set, init_num_clusters, centroids, method);
+        m_kmeans.clustering(dim, data_set, init_num_clusters, centroids, m_method);
     }
   
 }  // end of namespace scl
